d3ba06709173096287308b766697c6877690e3f8
Eduard Kuliev <36066152+mor1ins@users.noreply.github.com>
2020-11-14 21:49:21 +0300

    Сделал преобразование строки в массив чаров

--- example5.cpp
+++ example5.cpp
@@ -1,201 +1,3 @@
-#include <cstdio>
-#include <array>
-#include <vector>
-#include <numeric>
-#include <functional>
-#include <iostream>
-#include <ranges>
-
-
-namespace detail
-{
-    template<unsigned... digits>
-    struct to_chars { static const char value[]; };
-
-    template<unsigned... digits>
-    constexpr char to_chars<digits...>::value[] = {('0' + digits)..., 0};
-
-    template<unsigned rem, unsigned... digits>
-    struct explode : explode<rem / 10, rem % 10, digits...> {};
-
-    template<unsigned... digits>
-    struct explode<0, digits...> : to_chars<digits...> {};
-}
-
-template<unsigned num>
-struct num_to_string : detail::explode<num> {};
-
-
-template <typename T, size_t N, size_t M>
-consteval auto
-concat(const std::array<T, N> first, const std::array<T, M> second) {
-    std::array<T, N + M> result;
-    for (int i : std::ranges::views::iota(0, (int)(N + M))) {
-        result[i] = (i < N) ? first[i] : second[i - N];
-    }
-
-    return result;
-}
-
-template <size_t N, size_t M>
-consteval auto
-concat(const char (&first)[N], const char (&second)[M]) {
-    std::array<char, N + M - 1> result;
-    for (int i : std::ranges::views::iota(0, (int)(N + M - 1))) {
-        result[i] = (i < N - 1) ? first[i] : second[i - N + 1];
-    }
-
-    return result;
-}
-
-
-//template <size_t N>
-//struct reverse_range {
-//    explicit consteval reverse_range() {
-//        this->times = std::ranges::views::reverse(std::ranges::views::iota(0, (int)time));
-//    }
-//
-//    std::array<int, N> times;
-//};
-
-struct Signal {
-public:
-    consteval
-    explicit
-    Signal(std::size_t time = 0, bool is_pause = false)
-    : time(time), is_pause(is_pause)
-    {}
-
-    consteval Signal operator+(const Signal& other) const {
-        return Signal(this->time + other.time, this->is_pause && other.is_pause);
-    }
-
-    consteval void operator=(const Signal& other) {
-        if (this != &other) { // self-assignment check expected
-            this->time = other.time;
-            this->is_pause = other.is_pause;
-        }
-    }
-
-    consteval Signal operator+(const int other) const {
-        return Signal(this->time + other, this->is_pause);
-    }
-
-    [[nodiscard]] consteval Signal tick() const {
-        return Signal(time > 0 ? time - 1 : 0, is_pause);
-    }
-
-
-    std::size_t time;
-    bool is_pause;
-};
-
-
-struct Pause : Signal {
-    consteval Pause(size_t time) : Signal(time, true) {}
-};
-
-
-struct Vibrate : Signal {
-    consteval Vibrate(size_t time) : Signal(time) {}
-};
-
-
-template<size_t N = 0, typename ...Args>
-struct SignalSequence {
-public:
-    static consteval auto length() {
-        return N > 0 ? N : sizeof...(Args);
-    }
-
-//    consteval explicit SignalSequence(SignalSequence<N> sequence) : sequence(sequence.sequence) {}
-    consteval explicit SignalSequence(std::array<Signal, N> sequence) : sequence(sequence) {}
-    consteval explicit SignalSequence(Args... more) : sequence({more...}) {}
-
-    consteval auto sum() const {
-        int sum = 0;
-        for (auto signal: sequence) sum += signal.time;
-        return sum;
-    }
-
-    consteval auto operator+(const auto& other) const {
-        auto sequence = concat(this->sequence, other.sequence);
-        return SignalSequence<sequence.size()>(sequence);
-    }
-
-    std::array<Signal, length()> sequence;
-    std::string_view str = "hehehe";
-};
-
-//template<typename ...Args>
-//consteval auto sum(Args&&... args) {
-//    return std::array<std::string_view, sizeof...(Args)>{ (std::string_view(num_to_string<args>::value), ...) };
-//}
-
-//template <size_t N>
-//struct signals_to_strings {
-//    explicit consteval
-//    signals_to_strings()
-//    : value()
-//    {}
-//
-//    std::array<std::string_view, N> value = sum(1, 2, 3, 4, 5);
-//};
-
-
-consteval auto get_reverse_range(auto size) {
-    return std::ranges::views::reverse(std::ranges::views::iota(0, static_cast<int>(size)));
-}
-
-
-int main() {
-//    constexpr Signal start(10);
-//    constexpr Signal current(start.tick());
-//    volatile size_t i = current.get_time();
-//    std::cout << i << std::endl;
-//
-//    constexpr Signal current1(current.tick());
-//    i = current1.get_time();
-//    std::cout << i << std::endl;
-//
-//    constexpr Signal current2(current1.tick());
-//    i = current2.get_time();
-//    std::cout << i << std::endl;
-//
-//    constexpr Signal current3(current2.tick());
-//    i = current3.get_time();
-//    std::cout << i << std::endl;
-
-    constexpr SignalSequence first(Vibrate(1), Pause(2));
-    constexpr SignalSequence second(Vibrate(3), Pause(4), Vibrate(5));
-
-    constexpr auto sequence = first + second;
-
-    volatile auto sum = sequence.sum();
-    volatile auto length = sequence.length();
-
-    volatile size_t i = 0;
-    std::cout << "Start" << std::endl;
-    for (Signal signal: sequence.sequence) {
-        i = signal.time;
-//        printf("%s\n", concat(num_to_string<12>::value, num_to_string<34>::value));
-        std::cout << "Time: " << signal.time << ", "
-                  << (signal.is_pause ? "Waiting..." : "Vibrating...");
-
-        constexpr auto times = get_reverse_range(sequence.sequence[0].time);
-    }
-//    constexpr auto strs = signals_to_strings<sequence.length(), sequence, SignalSequence>().value;
-//    for (auto str : strs) std::cout << str << " ";
-    std::cout << std::endl << "End" << std::endl << std::endl;
-
-
-    std::cout << "sum: " << sum << "\n";
-    std::cout << "length: " << length << "\n";
-}
-
-
-
-
 //#include <cstdio>
 //#include <array>
 //#include <vector>
@@ -204,8 +6,29 @@ int main() {
 //#include <iostream>
 //#include <ranges>
 //
+//
+//namespace detail
+//{
+//    template<unsigned... digits>
+//    struct to_chars { static const char value[]; };
+//
+//    template<unsigned... digits>
+//    constexpr char to_chars<digits...>::value[] = {('0' + digits)..., 0};
+//
+//    template<unsigned rem, unsigned... digits>
+//    struct explode : explode<rem / 10, rem % 10, digits...> {};
+//
+//    template<unsigned... digits>
+//    struct explode<0, digits...> : to_chars<digits...> {};
+//}
+//
+//template<unsigned num>
+//struct num_to_string : detail::explode<num> {};
+//
+//
 //template <typename T, size_t N, size_t M>
-//consteval auto concat(const std::array<T, N> first, const std::array<T, M> second) {
+//consteval auto
+//concat(const std::array<T, N> first, const std::array<T, M> second) {
 //    std::array<T, N + M> result;
 //    for (int i : std::ranges::views::iota(0, (int)(N + M))) {
 //        result[i] = (i < N) ? first[i] : second[i - N];
@@ -214,41 +37,68 @@ int main() {
 //    return result;
 //}
 //
+//template <size_t N, size_t M>
+//consteval auto
+//concat(const char (&first)[N], const char (&second)[M]) {
+//    std::array<char, N + M - 1> result;
+//    for (int i : std::ranges::views::iota(0, (int)(N + M - 1))) {
+//        result[i] = (i < N - 1) ? first[i] : second[i - N + 1];
+//    }
+//
+//    return result;
+//}
+//
+//
+////template <size_t N>
+////struct reverse_range {
+////    explicit consteval reverse_range() {
+////        this->times = std::ranges::views::reverse(std::ranges::views::iota(0, (int)time));
+////    }
+////
+////    std::array<int, N> times;
+////};
+//
 //struct Signal {
 //public:
-//    consteval explicit Signal(std::size_t time = 0, bool is_pause = false)
-//        : time(time), is_pause(is_pause) {}
+//    consteval
+//    explicit
+//    Signal(std::size_t time = 0, bool is_pause = false)
+//    : time(time), is_pause(is_pause)
+//    {}
 //
 //    consteval Signal operator+(const Signal& other) const {
 //        return Signal(this->time + other.time, this->is_pause && other.is_pause);
+//    }
+//
+//    consteval void operator=(const Signal& other) {
+//        if (this != &other) { // self-assignment check expected
+//            this->time = other.time;
+//            this->is_pause = other.is_pause;
+//        }
 //    }
 //
 //    consteval Signal operator+(const int other) const {
 //        return Signal(this->time + other, this->is_pause);
 //    }
 //
-//    consteval std::size_t get_time() const {
-//        return time;
+//    [[nodiscard]] consteval Signal tick() const {
+//        return Signal(time > 0 ? time - 1 : 0, is_pause);
 //    }
 //
-//private:
+//
 //    std::size_t time;
 //    bool is_pause;
 //};
 //
 //
-////template<size_t N>
-////struct SignalSequence {
-////public:
-////    constexpr explicit SignalSequence(std::array<Signal, N> sequence) : sequence(sequence) {}
-////
-////    consteval auto get_sequence() const {
-////        return sequence;
-////    }
-////
-////private:
-////    std::array<Signal, N> sequence;
-////};
+//struct Pause : Signal {
+//    consteval Pause(size_t time) : Signal(time, true) {}
+//};
+//
+//
+//struct Vibrate : Signal {
+//    consteval Vibrate(size_t time) : Signal(time) {}
+//};
 //
 //
 //template<size_t N = 0, typename ...Args>
@@ -258,107 +108,257 @@ int main() {
 //        return N > 0 ? N : sizeof...(Args);
 //    }
 //
+////    consteval explicit SignalSequence(SignalSequence<N> sequence) : sequence(sequence.sequence) {}
 //    consteval explicit SignalSequence(std::array<Signal, N> sequence) : sequence(sequence) {}
 //    consteval explicit SignalSequence(Args... more) : sequence({more...}) {}
 //
 //    consteval auto sum() const {
 //        int sum = 0;
-//        for (auto signal: sequence) sum += signal.get_time();
+//        for (auto signal: sequence) sum += signal.time;
 //        return sum;
 //    }
 //
-//    consteval auto get_sequence() const {
-//        return sequence;
-//    }
-//
 //    consteval auto operator+(const auto& other) const {
-//        auto sequence = concat(this->get_sequence(), other.get_sequence());
+//        auto sequence = concat(this->sequence, other.sequence);
 //        return SignalSequence<sequence.size()>(sequence);
 //    }
 //
-//private:
 //    std::array<Signal, length()> sequence;
+//    std::string_view str = "hehehe";
 //};
 //
-////template <std::size_t N = 0>
-////struct NotificationQueue {
-////public:
-////    constexpr NotificationQueue(SignalSequence<N> sequence) : sequence(sequence) {}
-////    constexpr NotificationQueue() : sequence({Signal(0, false, true)}) {}
+////template<typename ...Args>
+////consteval auto sum(Args&&... args) {
+////    return std::array<std::string_view, sizeof...(Args)>{ (std::string_view(num_to_string<args>::value), ...) };
+////}
+//
+////template <size_t N>
+////struct signals_to_strings {
+////    explicit consteval
+////    signals_to_strings()
+////    : value()
+////    {}
 ////
-////    constexpr NotificationQueue notify(const auto sequence) {
-////        return NotificationQueue(this->sequence + sequence);
-////    }
-////
-////private:
-////    SignalSequence<N == 0 ? 1 : N> sequence;
+////    std::array<std::string_view, N> value = sum(1, 2, 3, 4, 5);
 ////};
 //
 //
+//consteval auto get_reverse_range(auto size) {
+//    return std::ranges::views::reverse(std::ranges::views::iota(0, static_cast<int>(size)));
+//}
+//
+//
 //int main() {
-//    constexpr SignalSequence first(Signal(5), Signal(5, true));
-//    constexpr SignalSequence second(Signal(5), Signal(5, true), Signal(5));
+////    constexpr Signal start(10);
+////    constexpr Signal current(start.tick());
+////    volatile size_t i = current.get_time();
+////    std::cout << i << std::endl;
+////
+////    constexpr Signal current1(current.tick());
+////    i = current1.get_time();
+////    std::cout << i << std::endl;
+////
+////    constexpr Signal current2(current1.tick());
+////    i = current2.get_time();
+////    std::cout << i << std::endl;
+////
+////    constexpr Signal current3(current2.tick());
+////    i = current3.get_time();
+////    std::cout << i << std::endl;
+//
+//    constexpr SignalSequence first(Vibrate(1), Pause(2));
+//    constexpr SignalSequence second(Vibrate(3), Pause(4), Vibrate(5));
 //
 //    constexpr auto sequence = first + second;
 //
 //    volatile auto sum = sequence.sum();
 //    volatile auto length = sequence.length();
 //
+//    volatile size_t i = 0;
+//    std::cout << "Start" << std::endl;
+//    for (Signal signal: sequence.sequence) {
+//        i = signal.time;
+////        printf("%s\n", concat(num_to_string<12>::value, num_to_string<34>::value));
+//        std::cout << "Time: " << signal.time << ", "
+//                  << (signal.is_pause ? "Waiting..." : "Vibrating...");
 //
-////    volatile NotificationQueue queue;
-////    volatile NotificationQueue new_queue = queue.notify(first);
+//        constexpr auto times = get_reverse_range(sequence.sequence[0].time);
+//    }
+////    constexpr auto strs = signals_to_strings<sequence.length(), sequence, SignalSequence>().value;
+////    for (auto str : strs) std::cout << str << " ";
+//    std::cout << std::endl << "End" << std::endl << std::endl;
+//
 //
 //    std::cout << "sum: " << sum << "\n";
 //    std::cout << "length: " << length << "\n";
 //}
-
-
-
-
 //
-//#include <cstdio>
-//#include <array>
-//#include <vector>
+//
+//
+//
+////#include <cstdio>
+////#include <array>
+////#include <vector>
+////#include <numeric>
+////#include <functional>
 ////#include <iostream>
+////#include <ranges>
+////
+////template <typename T, size_t N, size_t M>
+////consteval auto concat(const std::array<T, N> first, const std::array<T, M> second) {
+////    std::array<T, N + M> result;
+////    for (int i : std::ranges::views::iota(0, (int)(N + M))) {
+////        result[i] = (i < N) ? first[i] : second[i - N];
+////    }
+////
+////    return result;
+////}
+////
+////struct Signal {
+////public:
+////    consteval explicit Signal(std::size_t time = 0, bool is_pause = false)
+////        : time(time), is_pause(is_pause) {}
+////
+////    consteval Signal operator+(const Signal& other) const {
+////        return Signal(this->time + other.time, this->is_pause && other.is_pause);
+////    }
+////
+////    consteval Signal operator+(const int other) const {
+////        return Signal(this->time + other, this->is_pause);
+////    }
+////
+////    consteval std::size_t get_time() const {
+////        return time;
+////    }
+////
+////private:
+////    std::size_t time;
+////    bool is_pause;
+////};
+////
+////
+//////template<size_t N>
+//////struct SignalSequence {
+//////public:
+//////    constexpr explicit SignalSequence(std::array<Signal, N> sequence) : sequence(sequence) {}
+//////
+//////    consteval auto get_sequence() const {
+//////        return sequence;
+//////    }
+//////
+//////private:
+//////    std::array<Signal, N> sequence;
+//////};
+////
+////
+////template<size_t N = 0, typename ...Args>
+////struct SignalSequence {
+////public:
+////    static consteval auto length() {
+////        return N > 0 ? N : sizeof...(Args);
+////    }
+////
+////    consteval explicit SignalSequence(std::array<Signal, N> sequence) : sequence(sequence) {}
+////    consteval explicit SignalSequence(Args... more) : sequence({more...}) {}
+////
+////    consteval auto sum() const {
+////        int sum = 0;
+////        for (auto signal: sequence) sum += signal.get_time();
+////        return sum;
+////    }
+////
+////    consteval auto get_sequence() const {
+////        return sequence;
+////    }
+////
+////    consteval auto operator+(const auto& other) const {
+////        auto sequence = concat(this->get_sequence(), other.get_sequence());
+////        return SignalSequence<sequence.size()>(sequence);
+////    }
+////
+////private:
+////    std::array<Signal, length()> sequence;
+////};
+////
+//////template <std::size_t N = 0>
+//////struct NotificationQueue {
+//////public:
+//////    constexpr NotificationQueue(SignalSequence<N> sequence) : sequence(sequence) {}
+//////    constexpr NotificationQueue() : sequence({Signal(0, false, true)}) {}
+//////
+//////    constexpr NotificationQueue notify(const auto sequence) {
+//////        return NotificationQueue(this->sequence + sequence);
+//////    }
+//////
+//////private:
+//////    SignalSequence<N == 0 ? 1 : N> sequence;
+//////};
+////
+////
+////int main() {
+////    constexpr SignalSequence first(Signal(5), Signal(5, true));
+////    constexpr SignalSequence second(Signal(5), Signal(5, true), Signal(5));
+////
+////    constexpr auto sequence = first + second;
+////
+////    volatile auto sum = sequence.sum();
+////    volatile auto length = sequence.length();
+////
+////
+//////    volatile NotificationQueue queue;
+//////    volatile NotificationQueue new_queue = queue.notify(first);
+////
+////    std::cout << "sum: " << sum << "\n";
+////    std::cout << "length: " << length << "\n";
+////}
 //
 //
-//class Signal {
-//public:
-//    explicit Signal(std::size_t time, bool is_pause = false) : time(time), is_pause(is_pause) {}
 //
-//    Signal operator+(const Signal& other) const {
-//        return Signal(this->time + other.time, this->is_pause && other.is_pause);
-//    }
 //
-//    auto get_time() const {
-//        return time;
-//    }
-//
-//private:
-//    std::size_t time;
-//    bool is_pause;
-//};
-//
-//template<typename ...Args>
-//class SignalSequence {
-//public:
-//    explicit SignalSequence(Signal first, Args... more) : sequence({first, more...}) {}
-//
-//    [[nodiscard]] auto sum() const {
-//        std::size_t sum = 0;
-//
-//        for (auto signal: sequence)
-//            sum += signal.get_time();
-//
-//        return sum;
-//    }
-//
-//private:
-//    std::vector<Signal> sequence;
-//};
-//
-//int main() {
-//    SignalSequence sequence(Signal(5), Signal(5, true), Signal(5));
-//    volatile auto sum = sequence.sum();
-////    std::cout << signal.sum() << "\n";
-//}
+////
+////#include <cstdio>
+////#include <array>
+////#include <vector>
+//////#include <iostream>
+////
+////
+////class Signal {
+////public:
+////    explicit Signal(std::size_t time, bool is_pause = false) : time(time), is_pause(is_pause) {}
+////
+////    Signal operator+(const Signal& other) const {
+////        return Signal(this->time + other.time, this->is_pause && other.is_pause);
+////    }
+////
+////    auto get_time() const {
+////        return time;
+////    }
+////
+////private:
+////    std::size_t time;
+////    bool is_pause;
+////};
+////
+////template<typename ...Args>
+////class SignalSequence {
+////public:
+////    explicit SignalSequence(Signal first, Args... more) : sequence({first, more...}) {}
+////
+////    [[nodiscard]] auto sum() const {
+////        std::size_t sum = 0;
+////
+////        for (auto signal: sequence)
+////            sum += signal.get_time();
+////
+////        return sum;
+////    }
+////
+////private:
+////    std::vector<Signal> sequence;
+////};
+////
+////int main() {
+////    SignalSequence sequence(Signal(5), Signal(5, true), Signal(5));
+////    volatile auto sum = sequence.sum();
+//////    std::cout << signal.sum() << "\n";
+////}
--- example6.cpp
+++ example6.cpp
@@ -1,116 +1,132 @@
-//#include <string>
-//#include <array>
-//#include <iostream>
-//#include <ranges>
+#include <string>
+#include <array>
+#include <iostream>
+#include <ranges>
+#include <functional>
+
+consteval auto
+length(int number) {
+    int result = 0;
+    while (number > 0) {
+        result++;
+        number /= 10;
+    }
+    return result == 0 ? 1 : result;
+}
+
+template <typename T, size_t N>
+consteval auto
+reverse_array(std::array<T, N> arr) {
+    std::array<T, N> result{0};
+    for (int i = 0; i < N; i++) {
+        result[i] = arr[N - i - 1];
+    }
+    return result;
+}
+
+template <size_t N>
+consteval auto
+int_to_digits(int number) {
+    std::array<int, N> digits{0};
+    for (int i = 0; i < N; i++) {
+        digits[i] = number % 10;
+        number /= 10;
+    }
+    return reverse_array(digits);
+}
+
+template <int Digit>
+struct to_char {
+    char digit = '0' + Digit % 10;
+};
+
+consteval auto
+int_to_str(int number) {
+    std::array<std::string_view, 10> digits =
+            {"0", "1", "2", "3", "4",
+             "5", "6", "7", "8", "9"};
+
+    return digits[number % 10];
+}
+
+
+template <size_t N>
+consteval auto
+digits_to_strings(std::array<int, N> digits) {
+    std::array<char, N> result{0};
+    for (int i = 0; i < N; i++) result[i] = '0' + digits[i];
+    return result;
+}
+
+template <int N, int len = length(N)>
+consteval auto
+int_to_string() {
+    return digits_to_strings(int_to_digits<len>(N));
+}
+
+template <typename T, T get_delimiter>
+consteval auto str_to_chars() {
+    constexpr std::string_view delim = get_delimiter();
+    constexpr auto len = delim.size();
+    std::array<char, len> result{0};
+    for (int i = 0; i < len; i++) result[i] = delim[i];
+    return result;
+}
+
+
+template <size_t N, size_t M, size_t length = N + (N - 1) * M>
+consteval auto
+join(const std::array<char, M> delimiter, const std::array<char, N> sequence) {
+    std::array<char, length> result{0};
+
+    int sequence_index = 0;
+    int delimiter_index = 0;
+    for (int i : std::ranges::views::iota(0, (int)length)) {
+        if (delimiter_index == 0) {
+            result[i] = sequence[sequence_index++];
+            delimiter_index = M;
+        }
+        else {
+            result[i] = delimiter[M - delimiter_index];
+            delimiter_index -= 1;
+        }
+    }
+
+    return result;
+}
+
+template <size_t N, size_t M>
+consteval auto
+concat(const char * first, const char * second) {
+    std::array<char, N + M> result{0};
+    for (int i : std::ranges::views::iota(0, (int)(N + M))) {
+        result[i] = (i < N) ? first[i] : second[i - N];
+    }
+
+    return result;
+}
+
+template <char * Date, size_t N>
+struct compile_time_string_view {
+    std::string_view value {Date, N};
+};
+
+
+int main() {
+    constexpr std::array ints = int_to_string<1076>();
+    constexpr auto get_delimiter = [](){return ", ";};
+    constexpr std::array delimiter = str_to_chars<decltype(get_delimiter), get_delimiter>();
+    constexpr std::array ints_str = join(delimiter, ints);
+
+    for (auto i : ints_str) std::cout << i;
+    std::cout << std::endl;
+
+
+//    constexpr std::string_view a = "123";
+//    constexpr std::string_view b = "456";
+//    constexpr auto c = concat<a.size(), b.size()>(a.data(), b.data());
 //
-//
-//consteval auto length(int number) {
-//    int result = 0;
-//    while (number > 0) {
-//        result++;
-//        number /= 10;
-//    }
-//    return result == 0 ? 1 : result;
-//}
-//
-//template <typename T, size_t N>
-//consteval auto reverse_array(std::array<T, N> arr) {
-//    std::array<T, N> result{0};
-//    for (int i = 0; i < N; i++) {
-//        result[i] = arr[N - i - 1];
-//    }
-//    return result;
-//}
-//
-//template <size_t N>
-//consteval auto int_to_digits(int number) {
-//    std::array<int, N> digits{0};
-//    for (int i = 0; i < N; i++) {
-//        digits[i] = number % 10;
-//        number /= 10;
-//    }
-//    return reverse_array(digits);
-//}
-//
-//template <int Digit>
-//struct to_char {
-//    char digit = '0' + Digit % 10;
-//};
-//
-//consteval auto int_to_str(int number) {
-//    std::array<std::string_view, 10> digits =
-//            {"0", "1", "2", "3", "4",
-//             "5", "6", "7", "8", "9"};
-//
-//    return digits[number % 10];
-//}
-//
-//
-//template <size_t N>
-//consteval auto digits_to_strings(std::array<int, N> digits) {
-//    std::array<char, N> result{0};
-//    for (int i = 0; i < N; i++) result[i] = '0' + digits[i];
-//    return result;
-//}
-//
-//template <int N, int len = length(N)>
-//consteval auto int_to_string() {
-//    return digits_to_strings(int_to_digits<len>(N));
-//}
-////
-////
-//template <size_t N, size_t M, size_t length = N + (N - 1) * M>
-//consteval auto
-//join(const std::array<char, M> delimiter, const std::array<char, N> sequence) {
-//    std::array<char, length> result{0};
-//
-//    int sequence_index = 0;
-//    int delimiter_index = 0;
-//    for (int i : std::ranges::views::iota(0, (int)length)) {
-//        if (delimiter_index == 0) {
-//            result[i] = sequence[sequence_index++];
-//            delimiter_index = M;
-//        }
-//        else {
-//            result[i] = delimiter[M - delimiter_index];
-//            delimiter_index -= 1;
-//        }
-//    }
-//
-//    return result;
-//}
-//
-//template <size_t N, size_t M>
-//consteval auto
-//concat(const char * first, const char * second) {
-//    std::array<char, N + M> result{0};
-//    for (int i : std::ranges::views::iota(0, (int)(N + M))) {
-//        result[i] = (i < N) ? first[i] : second[i - N];
-//    }
-//
-//    return result;
-//}
-//
-//template <char * Date, size_t N>
-//struct compile_time_string_view {
-//    std::string_view value {Date, N};
-//};
-//
-//
-//int main() {
-//    constexpr std::array ints = int_to_string<1076>();
-//    constexpr std::array delimiter { ' ', '-', ' ' };
-//    constexpr std::array ints_str = join(delimiter, ints);
-//
-//    for (auto i : ints_str) std::cout << i;
-//    std::cout << std::endl;
-//
-//
-////    constexpr std::string_view a = "123";
-////    constexpr std::string_view b = "456";
-////    constexpr auto c = concat<a.size(), b.size()>(a.data(), b.data());
-////
-////    const char * str = c._M_elems;
-//
-//    return 0;
-//}
+//    const char * str = c._M_elems;
+
+    return 0;
+}
